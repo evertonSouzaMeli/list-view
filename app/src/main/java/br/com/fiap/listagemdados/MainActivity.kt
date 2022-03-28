62f0b62165a07d9c9ff42a4873992d6d0f0f9879
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-03-27 23:01:32 -0300

    start of project

---
+++ app/src/main/java/br/com/fiap/listagemdados/MainActivity.kt
@@ -0,0 +1,56 @@
+package br.com.fiap.listagemdados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        lista()
+    }
+
+    fun lista(){
+
+        var listaEstados = arrayListOf(
+            "Acre - AC",
+            "Alagoas - AL",
+            "Amapá - AP",
+            "Amazonas - AM",
+            "Bahia - BA",
+            "Ceará - CE",
+            "Distrito Federal - DF",
+            "Espírito Santo - ES",
+            "Goiás - GO",
+            "Maranhão - MA",
+            "Mato Grosso - MT",
+            "Mato Grosso do Sul - MS",
+            "Minas Gerais - MG",
+            "Pará - PA",
+            "Paraíba - PB",
+            "Paraná - PR",
+            "Pernambuco - PE",
+            "Piauí - PI",
+            "Rio de Janeiro - RJ",
+            "Rio Grande do Norte - RN",
+            "Rio Grande do Sul - RS",
+            "Rondônia - RO",
+            "Roraima - RR",
+            "Santa Catarina - SC",
+            "São Paulo - SP",
+            "Sergipe - SE",
+            "Tocantins - TO"
+        )
+
+        var listaViewEstados = findViewById<ListView>(R.id.listViewEstados)
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaEstados)
+        listaViewEstados.adapter = adapter
+
+        listaViewEstados.setOnItemClickListener{ parent, view, position, id ->
+            Toast.makeText(this, listaEstados[position], Toast.LENGTH_LONG).show()
+        }
+    }
+}
